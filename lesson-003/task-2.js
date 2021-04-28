64ecb87cd4fd03195c8c4200a3b0eb5663635298
Arthur <gyulabyana@gmail.com>
2021-04-28 16:12:19 +0400

    Second task completed

---
+++ lesson-003/task-2.js
@@ -0,0 +1,7 @@
+// Task 2
+
+let i = 10;
+let n = i++ % 5;
+
+// Value of n will be 0, 'cause the postfix increment operator returns the initial value of operand, then does the operation.
+console.log(i, n); // Output -> 11 0
