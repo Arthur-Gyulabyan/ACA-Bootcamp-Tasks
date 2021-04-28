5ff0a0a52536487a0eac62fa730b4d3db5f5909c
Arthur <gyulabyana@gmail.com>
2021-04-28 15:53:36 +0400

    First task completed

---
+++ task-003/task-1.js
@@ -0,0 +1,10 @@
+// Declaring variables
+let admin;
+let name;
+
+// Assigning values to the variables
+name = "John";
+admin = name;
+
+// Logging the value of "admin"
+console.log(admin); // Output -> "John"
