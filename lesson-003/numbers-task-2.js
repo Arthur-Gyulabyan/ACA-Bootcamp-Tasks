dc7ed60c0e91694862328a03f07ba9de9a9219f9
Arthur <gyulabyana@gmail.com>
2021-04-28 22:44:30 +0400

    solve: numbers task 2 completed

---
+++ lesson-003/numbers-task-2.js
@@ -0,0 +1,17 @@
+// Repeat until input is a number
+
+function readNumber() {
+  let num = prompt("Please, enter a number!");
+
+  if (num === null || num === "") {
+    // Checking for cancel and empty string input
+    return null;
+  } else if (+num !== +num) {
+    // Checking for invalid input
+    return readNumber(); // Will be executed, until valid input
+  }
+
+  return +num;
+}
+
+alert(readNumber());
