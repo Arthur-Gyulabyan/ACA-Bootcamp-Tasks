b6b222eb65c680d672d8eafd5180d230185b89c9
Arthur <gyulabyana@gmail.com>
2021-04-28 18:21:42 +0400

    solve: logical operators task 2 completed

---
+++ lesson-003/logical-operators-task-2.js
@@ -0,0 +1,11 @@
+// Check the range
+
+const age = 12;
+
+if (!(age >= 14 && age <= 90)) {
+  console.log("Age is out of range!");
+}
+
+if (age < 14 || age > 90) {
+  console.log("Age is out of range!");
+}
