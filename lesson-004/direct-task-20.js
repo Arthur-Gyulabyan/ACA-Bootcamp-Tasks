ef8271fc91b9da0cd69d8d5933faeffd7cf20ca5
Arthur <gyulabyana@gmail.com>
2021-04-29 23:31:10 +0400

    feat: add a function to task 20

--- lesson-004/direct-task-20.js
+++ lesson-004/direct-task-20.js
@@ -2,6 +2,7 @@
 
 const arr = [[], [], [], [], []];
 
+/*
 // Solution by 2 for loops
 let counter = 0;
 
@@ -17,6 +18,7 @@ console.log(arr);
 for (let i = 0; i < arr.length; i++) {
   console.log(...arr[i]);
 }
+*/
 
 /*
 //  Solution by while and for loops
@@ -32,3 +34,28 @@ while (i < 16) {
   counter++;
 }
 */
+
+// Function will draw a pyramid according to given level count.
+function drawPyramid(levelCount) {
+  const arr = [];
+
+  for (let i = 0; i < levelCount; i++) {
+    arr.push([]);
+  }
+
+  let counter = 0;
+
+  for (let i = 1; i <= ((1 + levelCount) / 2) * levelCount; ) {
+    for (let j = counter; j < arr.length; j++) {
+      arr[j].push(i);
+      i++;
+    }
+    counter++;
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    console.log(...arr[i]);
+  }
+}
+
+drawPyramid(8);
