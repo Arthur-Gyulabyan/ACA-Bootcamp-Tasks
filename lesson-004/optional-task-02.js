1ccae90bf86201c75bf67d2d730636e5d480e8f7
Arthur <gyulabyana@gmail.com>
2021-04-30 17:33:38 +0400

    solve: optional task 2 completed

---
+++ lesson-004/optional-task-02.js
@@ -0,0 +1,17 @@
+// Convert base-2 number to base-10
+
+function binaryToDecimal(binaryNum) {
+  // Get reversed array of binary number digits
+  const digitsArr = binaryNum.toString().split('').reverse();
+
+  let decimalNum = 0;
+
+  // Convert binary to decimal
+  for (let i = 0; i < digitsArr.length; i++) {
+    decimalNum += +digitsArr[i] * 2 ** i;
+  }
+  return decimalNum;
+}
+
+console.log(binaryToDecimal(101110)); // 46
+console.log(binaryToDecimal(101)); // 5
