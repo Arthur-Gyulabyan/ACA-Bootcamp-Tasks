0caf8d69d7d6efcc5526de0e4d0b6d071aa28406
Arthur <gyulabyana@gmail.com>
2021-05-03 00:37:13 +0400

    solve: loops task 2 completed

---
+++ lesson-004/loops-task-02.js
@@ -0,0 +1,9 @@
+// Which values does the while loop show?
+
+// The prefix
+let i = 0;
+while (++i < 5) alert(i); // 1 -> 2 -> 3 -> 4
+
+// The postfix
+let j = 0;
+while (j++ < 5) alert(j); // 1 -> 2 -> 3 -> 4 -> 5
