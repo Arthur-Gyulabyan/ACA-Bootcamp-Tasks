8bdd1a007093f59595d0420c1c11106f156cab9c
Arthur <gyulabyana@gmail.com>
2021-05-03 01:11:30 +0400

    solve: loops task 5 completed

---
+++ lesson-004/loops-task-05.js
@@ -0,0 +1,12 @@
+// Replace "for" with "while"
+
+// // Rewrite the following "for" loop using "while".
+// for (let i = 0; i < 3; i++) {
+//   console.log(`number ${i}!`);
+// }
+
+let i = 0;
+while (i < 3) {
+  console.log(`number ${i}!`);
+  i++;
+}
