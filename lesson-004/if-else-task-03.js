2cc398d7e1c07b2fe51b23cbc5d3c4815ae68f10
Arthur <gyulabyana@gmail.com>
2021-05-02 21:00:01 +0400

    solve: conditional branching task 3 completed

---
+++ lesson-004/if-else-task-03.js
@@ -0,0 +1,11 @@
+// Show the sign (we assume that the input is always a number)
+
+let number = prompt('Insert a number!');
+
+if (number > 0) {
+  alert(1);
+} else if (number < 0) {
+  alert(-1);
+} else {
+  alert(0);
+}
