9bb7d0811805d8a3ab79c2254e1a95f2982bf0b9
Arthur <gyulabyana@gmail.com>
2021-05-03 00:00:11 +0400

    solve: loops task 1 completed

---
+++ lesson-004/loops-task-01.js
@@ -0,0 +1,10 @@
+// Last loop value
+
+// What is the last value alerted by this code? Why?
+let i = 3;
+
+while (i) {
+  alert(i--);
+}
+// Last output will be 1, 'cause postfix decrement operator returns the value then does the operation.
+// Sequence of output will be 3 -> 2 -> 1
