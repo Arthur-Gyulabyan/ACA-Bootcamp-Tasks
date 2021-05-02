4af1e2c474cec3bd3deded2606f981231dd42791
Arthur <gyulabyana@gmail.com>
2021-05-03 02:27:28 +0400

    solve: loops task 6 completed

---
+++ lesson-004/loops-task-06.js
@@ -0,0 +1,7 @@
+// Repeat until the input is correct
+
+let number = prompt('Insert a number greater than 100!');
+
+while (number <= 100 && number) {
+  number = prompt('Insert a number greater than 100!');
+}
