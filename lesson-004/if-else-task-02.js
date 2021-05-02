670802c210993b0ec231ede324b2c70c938f09ee
Arthur <gyulabyana@gmail.com>
2021-05-02 19:00:28 +0400

    solve: conditional branching task 2 completed

---
+++ lesson-004/if-else-task-02.js
@@ -0,0 +1,9 @@
+// The name of JavaScript
+
+const answer = prompt('What is the “official” name of JavaScript?');
+
+if (answer.toLowerCase() === 'ecmascript') {
+  alert('Right!');
+} else {
+  alert('You don’t know? ECMAScript!');
+}
