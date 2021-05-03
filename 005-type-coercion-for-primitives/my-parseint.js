d038951459fcb5da3f130aa6ebaf57d345a5c435
Arthur <gyulabyana@gmail.com>
2021-05-03 18:20:40 +0400

    fix: the issue about spaces in string fixed

--- 005-type-coercion-for-primitives/my-parseint.js
+++ 005-type-coercion-for-primitives/my-parseint.js
@@ -3,8 +3,9 @@
 function myParseInt(str) {
   let numString = '';
 
-  for (let char of str) {
-    if (!isNaN(char)) {
+  // We need to use "trim()" on our string to avoid cases when string starts with space.
+  for (let char of str.trim()) {
+    if (!isNaN(char) && char !== ' ') {
       numString += char;
     } else {
       break;
@@ -17,4 +18,4 @@ function myParseInt(str) {
 console.log(myParseInt('a0011b1')); // NaN
 console.log(myParseInt('23.231')); // 23
 console.log(myParseInt('18px')); // 18
-console.log(myParseInt('')); // NaN
+console.log(myParseInt('  00 1')); // 0
