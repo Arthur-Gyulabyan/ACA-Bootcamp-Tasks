d5ae6f91482fe879f9155d08da0127c2a28cb4a3
Arthur <gyulabyana@gmail.com>
2021-05-03 17:22:27 +0400

    solve: parseFloat implementation task completed

---
+++ 005-type-coercion-for-primitives/my-parsefloat.js
@@ -0,0 +1,34 @@
+// Write logic that implements parseFloat
+
+function myParseFloat(str) {
+  let numString = '';
+  let firstDotFound = false; // This variable will indicate, whether the first dot appeared or not
+
+  for (let char of str) {
+    if (char === '.') {
+      // Check if the current dot is the first in the string or not
+      if (firstDotFound) {
+        // If not the first one
+        break;
+      } else {
+        // If element is the first dot in string
+        numString += '.';
+        firstDotFound = true;
+        continue;
+      }
+    }
+
+    if (!isNaN(char)) {
+      numString += char;
+    } else {
+      break;
+    }
+  }
+
+  return numString ? Number(numString) : NaN;
+}
+
+console.log(myParseFloat('11.5cm')); // 11.5
+console.log(myParseFloat('23.a231')); // 23
+console.log(myParseFloat('18.2.p3px.231.23')); // 18.2
+console.log(myParseFloat('')); // NaN
