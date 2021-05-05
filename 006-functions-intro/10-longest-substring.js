68ad0a03f1ee3438e5c733b5af88bb3c0af81358
Arthur <gyulabyana@gmail.com>
2021-05-05 23:51:34 +0400

    fix: task 10 - some redundant code removed and bugs fixed

--- 006-functions-intro/10-longest-substring.js
+++ 006-functions-intro/10-longest-substring.js
@@ -6,23 +6,27 @@ function findLongestSubstr(str) {
 
   for (let i = 0; i < str.length; i++) {
     if (currentLongest.indexOf(str[i]) === -1 || str[i] === ' ') {
+      // If NO repeat found
       currentLongest += str[i];
       continue;
     }
 
-    if (currentLongest.indexOf(str[i]) !== -1) {
-      if (currentLongest.length >= longestSubstr.length) {
-        longestSubstr = currentLongest;
-        currentLongest = '';
-      } else {
-          i -= currentLongest.length;
-          currentLongest = '';
-      }
+    if (currentLongest.length >= longestSubstr.length) {
+      // If repeat found
+      longestSubstr = currentLongest;
+      i -= currentLongest.length;
+      currentLongest = '';
+    } else {
+      i -= currentLongest.length;
+      currentLongest = '';
     }
   }
 
-  return longestSubstr;
+  return longestSubstr ? longestSubstr : str;
 }
 
-console.log(findLongestSubstr('there are no two words in the english language more harmful than "good job".'));
+console.log(
+  findLongestSubstr('there are no two words in the english language more harmful than "good job".')
+);
 console.log(findLongestSubstr('parting your soup is not a miracle, bruce.'));
+console.log(findLongestSubstr('the big dwarf only jumps.'));
