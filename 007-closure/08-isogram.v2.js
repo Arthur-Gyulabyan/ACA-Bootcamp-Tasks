27e7e6877e9b068f419aba1bfe48dfb1e5568e74
Arthur <gyulabyana@gmail.com>
2021-05-06 16:06:31 +0400

    feat: task 8 implemented using one loop

---
+++ 007-closure/08-isogram.v2.js
@@ -0,0 +1,17 @@
+// Implementation with one loop
+function checkIsogram(word) {
+  const lowerCaseWord = word.toLowerCase();
+  let isIsogram = true;
+
+  for (let i = 0; i < lowerCaseWord.length; i++) {
+    if (lowerCaseWord.slice(i + 1).indexOf(lowerCaseWord[i]) !== -1) {
+      isIsogram = false;
+    }
+  }
+
+  return isIsogram ? `"${word}" is an isogram!` : `"${word}" is NOT an isogram!`;
+}
+
+console.log(checkIsogram('Arthur')); // "Arthur" is NOT an isogram!
+console.log(checkIsogram('prefix')); // "prefix" is an isogram!
+console.log(checkIsogram('beaUtiful')); // "beaUtiful" is NOT an isogram!
