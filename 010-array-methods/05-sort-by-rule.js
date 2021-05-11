078e56181f890d63922863020fb02424518f18f6
Arthur <gyulabyana@gmail.com>
2021-05-11 19:16:13 +0400

    fix: task 5 - case of not passing rule fixed

--- 010-array-methods/05-sort-by-rule.js
+++ 010-array-methods/05-sort-by-rule.js
@@ -1,6 +1,6 @@
 // Function takes array and string and returns the array sorted following the rule
 
-const sortByRule = (arr, rule) => {
+const sortByRule = (arr, rule = 'Asc') => {
   const arrCopy = [...arr]; // In order not to mutate the initial array.
 
   if (rule === 'Asc') {
@@ -10,10 +10,8 @@ const sortByRule = (arr, rule) => {
   if (rule === 'Desc') {
     return arrCopy.sort((a, b) => b - a); // in descending order
   }
-
-  return arrCopy;
 };
 
 console.log(sortByRule([4, 3, 2, 1], 'Asc')); // [1, 2, 3, 4]
-console.log(sortByRule([7, 8, 11, 66])); // [7, 8, 11, 66]
+console.log(sortByRule([7, 8, 1, 11, 66])); // [1, 7, 8, 11, 66]
 console.log(sortByRule([7, 8, 11, 66], 'Desc')); // [66, 11, 8, 7]
