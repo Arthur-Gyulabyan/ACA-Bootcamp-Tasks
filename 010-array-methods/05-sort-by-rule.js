91baf531d87f169da1f7168b061dad7d181ef927
Arthur <gyulabyana@gmail.com>
2021-05-11 17:26:14 +0400

    fix: task 5 - mutation of initial array prevented

--- 010-array-methods/05-sort-by-rule.js
+++ 010-array-methods/05-sort-by-rule.js
@@ -1,15 +1,17 @@
 // Function takes array and string and returns the array sorted following the rule
 
 const sortByRule = (arr, rule) => {
+  const arrCopy = [...arr]; // In order not to mutate the initial array.
+
   if (rule === 'Asc') {
-    return arr.sort((a, b) => a - b); // in ascending order
+    return arrCopy.sort((a, b) => a - b); // in ascending order
   }
 
   if (rule === 'Desc') {
-    return arr.sort((a, b) => b - a); // in descending order
+    return arrCopy.sort((a, b) => b - a); // in descending order
   }
 
-  return arr;
+  return arrCopy;
 };
 
 console.log(sortByRule([4, 3, 2, 1], 'Asc')); // [1, 2, 3, 4]
