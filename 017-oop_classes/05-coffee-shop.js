04cec83c827434140a16ca47b9df42562061b961
Arthur <gyulabyana@gmail.com>
2021-05-24 23:32:25 +0400

    feat: task 5 - first testing, "fulfillOrder()" method created

--- 017-oop_classes/05-coffee-shop.js
+++ 017-oop_classes/05-coffee-shop.js
@@ -16,3 +16,32 @@ CoffeeShop.prototype.addOrder = function (orderName) {
         console.log('This item is currently unavailable!');
     }
 };
+
+CoffeeShop.prototype.fulfillOrder = function () {
+    if (this.order.length !== 0) {
+        const ready = this.order.shift();
+        console.log(`The ${ready} is ready!`);
+    } else {
+        console.log('All orders have been fulfilled!');
+    }
+};
+
+const menu = [
+    { name: 'Pasta', type: 'food', price: 2500 },
+    { name: 'Kebab', type: 'food', price: 800 },
+    { name: 'Pizza', type: 'food', price: 2000 },
+    { name: 'Mojito', type: 'drink', price: 1000 },
+    { name: 'Margarita', type: 'drink', price: 2500 },
+];
+
+const seasons = new CoffeeShop('Pekin', menu);
+
+seasons.addOrder('Pizza');
+seasons.addOrder('Margarita');
+seasons.addOrder('Shawarma');
+seasons.fulfillOrder();
+seasons.fulfillOrder();
+seasons.addOrder('Kebab');
+
+seasons.fulfillOrder();
+console.log(seasons);
