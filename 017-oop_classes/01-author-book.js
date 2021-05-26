8b6986ad96ea7506342be20413192afbc38362e7
Arthur <gyulabyana@gmail.com>
2021-05-26 21:32:37 +0400

    feat: task 1 "author & book" - Book class created

--- 017-oop_classes/01-author-book.js
+++ 017-oop_classes/01-author-book.js
@@ -4,7 +4,7 @@ class Author {
     constructor(name, email, gender) {
         this._name = name;
         this._email = email;
-        this._gender = gender;
+        this._gender = gender.toLowerCase();
     }
 
     get name() {
@@ -59,3 +59,81 @@ class Author {
         }
     }
 }
+
+class Book {
+    constructor(title, author, price, quantity) {
+        this._title = title;
+        this._author = author;
+        this._price = price;
+        this._quantity = quantity;
+    }
+
+    get title() {
+        return this._title;
+    }
+
+    set title(value) {
+        if (typeof value !== 'string') {
+            throw new Error('Invalid title!');
+        }
+
+        this._title = value;
+    }
+
+    get author() {
+        return this._author;
+    }
+
+    set author(value) {
+        if (typeof value !== 'string') {
+            throw new Error('Invalid author name!');
+        }
+
+        this._author = value;
+    }
+
+    get price() {
+        return this._price;
+    }
+
+    set price(value) {
+        if (typeof value !== 'number') {
+            throw new Error('Invalid price value!');
+        }
+
+        this._price = value;
+    }
+
+    get quantity() {
+        return this._quantity;
+    }
+
+    set quantity(value) {
+        if (typeof value !== 'number' || Number.isNaN(value)) {
+            throw new Error('Invalid quantity value!');
+        }
+
+        this._quantity = Math.trunc(value);
+    }
+
+    getProfit() {
+        return this.price * this.quantity;
+    }
+
+    toString() {
+        return `${this.author} - ${this.title}`;
+    }
+}
+
+const author1 = new Author('J. K. Rowling', 'abc@gmail.com', 'fEmaLe');
+console.log(author1.gender); // female
+console.log(author1.name); // J. K. Rowling
+console.log(author1.toString()); // Ms. J. K. Rowling
+// author1.name = 12; // Invalid name!
+
+const book1 = new Book('Harry Potter', author1, 35, 356);
+console.log(book1.author);
+console.log(book1.price); // 35
+console.log(book1.quantity); // 356
+console.log(book1.getProfit()); // 12460
+book1.quantity = 'abc';
