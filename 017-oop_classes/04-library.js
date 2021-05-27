f9faf855af1948ca469cd3fd6ba6dc1fd9840bda
Arthur <gyulabyana@gmail.com>
2021-05-27 16:03:05 +0400

    feat: task 4 "library" - LibraryBookBase class created

--- 017-oop_classes/04-library.js
+++ 017-oop_classes/04-library.js
@@ -4,7 +4,11 @@ class Book {
     constructor(title, author) {
         this._title = title;
         this._author = author;
+
+        Book.idCounter++;
     }
+
+    static idCounter = 0;
 
     get title() {
         return this._title;
@@ -25,3 +29,18 @@ class Book {
 
 const book1 = new Book("You don't know JS", 'Kyle Simpson');
 console.log(book1.title);
+
+class LibraryBookBase extends Book {
+    constructor(title, author) {
+        super(title, author);
+        this._bookId = Book.idCounter;
+    }
+
+    toString() {
+        return `"${this.title}" - ${this.author}. ID - ${this.bookId}`;
+    }
+}
+
+const baseBook1 = new LibraryBookBase('Harry Potter', 'J. K. Rowling');
+console.log(baseBook1.title);
+console.log(baseBook1.author);
