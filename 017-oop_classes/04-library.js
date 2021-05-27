5a1cc83f229e59e674a122e8a3046c65b3f389eb
Arthur <gyulabyana@gmail.com>
2021-05-27 17:19:19 +0400

    feat: task 4 "library" - LibraryBook subclass created

--- 017-oop_classes/04-library.js
+++ 017-oop_classes/04-library.js
@@ -36,6 +36,10 @@ class LibraryBookBase extends Book {
         this._bookId = Book.idCounter;
     }
 
+    get bookId() {
+        return this._bookId;
+    }
+
     toString() {
         return `"${this.title}" - ${this.author}. ID - ${this.bookId}`;
     }
@@ -44,3 +48,41 @@ class LibraryBookBase extends Book {
 const baseBook1 = new LibraryBookBase('Harry Potter', 'J. K. Rowling');
 console.log(baseBook1.title);
 console.log(baseBook1.author);
+
+class LibraryBook extends Book {
+    constructor(title, author, quantity) {
+        super(title, author);
+        this._bookId = Book.idCounter;
+        this._quantity = quantity;
+    }
+
+    get quantity() {
+        return this._quantity;
+    }
+
+    set quantity(value) {
+        if (typeof value !== 'number' || Number.isNaN(value) || value < 0) {
+            throw new Error('Invalid quantity value!');
+        }
+
+        this._quantity = Math.trunc(value);
+    }
+
+    increaseQuantityBy(amount) {
+        this.quantity += amount;
+        return this.quantity;
+    }
+
+    decreaseQuantityBy(amount) {
+        this.quantity -= amount;
+        return this.quantity;
+    }
+
+    toString() {
+        return `"${this.title}" - ${this.author}. Quantity - ${this.quantity}`;
+    }
+}
+
+const libraryBook1 = new LibraryBook('The Old Man and the Sea', 'Ernest Hemingway', 12);
+console.log(libraryBook1.increaseQuantityBy(12));
+console.log(libraryBook1.decreaseQuantityBy(24));
