e7af13b517b423b6121c158557d596e863b7ef14
Arthur <gyulabyana@gmail.com>
2021-05-31 21:11:24 +0400

    solve: "add parentheses" task done

---
+++ 018-algorithms_ds_recursion/add-parentheses.js
@@ -0,0 +1,14 @@
+// Add brackets between letters.
+
+const addBrackets = (str) => {
+    if (str.length === 1 || str.length === 2) {
+        return str;
+    }
+
+    return `${str[0]}(` + addBrackets(str.slice(1, str.length - 1)) + `)${str[str.length - 1]}`;
+};
+
+const str1 = 'LItBeoFLcSGBOFQxMHoIuDDWcqcVgkcRoAeocXO';
+const str2 = 'LItBeoFLcSGBOFQxMHoIIuDDWcqcVgkcRoAeocXO';
+console.log(addBrackets(str1));
+console.log(addBrackets(str2));
