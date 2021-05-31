219be30e818ac4efcc2462285f2c72f07024ac48
Arthur <gyulabyana@gmail.com>
2021-05-31 22:39:59 +0400

    solve: task 1 "is all odd" done

---
+++ 018-algorithms_ds_recursion/01-is-all-odd.js
@@ -0,0 +1,24 @@
+// Write a recursive function to determine whether all digits of the number are odd or not.
+
+function isAllOdd(number) {
+    function iter(number, bool) {
+        const last = number % 10;
+        bool = last % 2 === 1;
+
+        if (bool === false) {
+            return false;
+        }
+
+        if (number < 10) {
+            return bool;
+        }
+
+        return iter((number - last) / 10, bool);
+    }
+
+    return iter(number, true);
+}
+
+console.log(isAllOdd(4211133)); // false
+console.log(isAllOdd(7791)); // true
+console.log(isAllOdd(5)); // true
