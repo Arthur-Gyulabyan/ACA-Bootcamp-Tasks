4a163677e8374ab8d279bf79c9967b9a834ca58e
Arthur <gyulabyana@gmail.com>
2021-05-31 23:14:25 +0400

    solve: task 2 "minimal positive element" done

---
+++ 018-algorithms_ds_recursion/02-minimal-positive.js
@@ -0,0 +1,24 @@
+// Write a recursive function to find its minimal positive element in the given array.
+
+function findMinimal(numbers) {
+    function iter(numbers, min) {
+        if (numbers.length === 0) {
+            return min === Infinity ? -1 : min;
+        }
+
+        const [first, ...rest] = numbers;
+        min = first < min && first >= 0 ? first : min;
+
+        return iter(rest, min);
+    }
+
+    return iter(numbers, Infinity);
+}
+
+const numbers1 = [56, -9, 87, -23, 0, -105, 55, 1];
+const numbers2 = [45, -9, 15, 5, -78];
+const numbers3 = [-5, -9, -111, -1000, -7];
+
+console.log(findMinimal(numbers1)); // 0
+console.log(findMinimal(numbers2)); // 5
+console.log(findMinimal(numbers3)); // -1
