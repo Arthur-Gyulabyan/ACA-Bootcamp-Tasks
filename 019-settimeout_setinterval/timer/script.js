41329a715d3d5b947e5ac26fa6df6d59735a2e34
Arthur <gyulabyana@gmail.com>
2021-06-22 01:58:26 +0400

    feat: functionality of changing timer values added

--- 019-settimeout_setinterval/timer/script.js
+++ 019-settimeout_setinterval/timer/script.js
@@ -58,3 +58,108 @@ const resetTimer = () => {
 start.addEventListener('click', decrease);
 stop.addEventListener('click', stopTimer);
 reset.addEventListener('click', resetTimer);
+
+
+// Change timer
+const hourInput = document.querySelector('.hour-input');
+const minuteInput = document.querySelector('.minute-input');
+const secInput = document.querySelector('.sec-input');
+const confirm = document.querySelector('.confirm');
+
+
+// Setting input value's maximum length to 2
+[hourInput, minuteInput, secInput].forEach(el => {
+    el.addEventListener('input', (event) => {
+        if (event.target.value.length > 2) {
+            event.target.value = event.target.value.slice(0, 2);
+        }
+    });
+});
+
+// Block '+, -, ., e' in input fields.
+[hourInput, minuteInput, secInput].forEach(el => {
+    const invalidChars = ['-', '+', 'e', '.'];
+
+    el.addEventListener('keydown', function (e) {
+        if (invalidChars.includes(e.key)) {
+            e.preventDefault();
+        }
+    });
+});
+
+// [hour, minute, sec].forEach(el => {
+//     el.addEventListener('click', (event) => {
+//         confirm.style.display = 'block';
+//
+//         if (event.target === hour) {
+//             hour.style.display = 'none';
+//             hourInput.classList.remove('input');
+//             hourInput.focus();
+//             hourInput.value = '';
+//             hourInput.value = `${hour.textContent}`;
+//         }
+//
+//         if (event.target === minute) {
+//             minute.style.display = 'none';
+//             minuteInput.classList.remove('input');
+//             minuteInput.focus();
+//             minuteInput.value = '';
+//             minuteInput.value = `${minute.textContent}`;
+//         }
+//
+//         if (event.target === sec) {
+//             sec.style.display = 'none';
+//             secInput.classList.remove('input');
+//             secInput.focus();
+//             secInput.value = '';
+//             secInput.value = `${sec.textContent}`;
+//         }
+//     });
+// });
+
+hour.addEventListener('click', () => {
+    hour.style.display = 'none';
+    hourInput.classList.remove('input');
+    hourInput.focus();
+    hourInput.value = '';
+    hourInput.value = `${hour.textContent}`;
+});
+
+minute.addEventListener('click', () => {
+    minute.style.display = 'none';
+    minuteInput.classList.remove('input');
+    minuteInput.focus();
+    minuteInput.value = '';
+    minuteInput.value = `${minute.textContent}`;
+});
+
+sec.addEventListener('click', () => {
+    sec.style.display = 'none';
+    secInput.classList.remove('input');
+    secInput.focus();
+    secInput.value = '';
+    secInput.value = `${sec.textContent}`;
+});
+
+[hourInput, minuteInput, secInput].forEach(el => {
+    el.addEventListener('focusout', (event) => {
+        const value = event.target.value;
+
+        if (value === '') {
+            event.target.value = '00';
+        }
+
+        if (value.length === 1) {
+            event.target.value = `0${event.target.value}`;
+        }
+
+        if (event.target === minuteInput && value > 59 || event.target === secInput && value > 59) {
+            event.target.value = '59';
+        }
+
+        event.target.previousElementSibling.style.display = 'inline';
+        event.target.classList.add('input');
+        event.target.previousElementSibling.textContent = event.target.value;
+    });
+});
+
