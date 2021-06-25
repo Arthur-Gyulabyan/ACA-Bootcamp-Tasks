8bd6da6bfe5f1c8adb47a41e3f405d5800d6c402
Arthur <gyulabyana@gmail.com>
2021-06-25 21:57:09 +0400

    feat: functionality of getting all breed names from api added

--- 022-fetch-api/dog-breeds/index.js
+++ 022-fetch-api/dog-breeds/index.js
@@ -1,11 +1,32 @@
-const select = document.querySelector('.select');
+const select = document.querySelector('#breeds');
 const imgContainer = document.querySelector('.img-container');
+
+const defineSelect = (data) => {
+    const nameArr = Object.keys(data.message);
+    console.log(nameArr);
+
+    nameArr.forEach(el => {
+        const option = document.createElement('option');
+        option.setAttribute('value', el);
+        option.textContent = el[0].toUpperCase() + el.slice(1);
+
+        select.append(option);
+    });
+};
+
+const getBreedNames = () => {
+    fetch('https://dog.ceo/api/breeds/list/all')
+        .then(response => response.json())
+        .then(data => defineSelect(data));
+};
+
 
 const renderImages = (data) => {
     imgContainer.innerHTML = '';
     data.message.slice(0, 30).forEach(el => {
         const img = document.createElement('img');
         img.src = el;
+        imgContainer.style.display = 'flex';
         imgContainer.append(img);
     });
 };
@@ -17,6 +38,9 @@ const getBreedData = (breed) => {
         .catch(reject => console.log(reject));
 };
 
+
+// Event listeners
+window.addEventListener('load', getBreedNames);
 
 select.addEventListener('change', () => {
     if (select.value) {
