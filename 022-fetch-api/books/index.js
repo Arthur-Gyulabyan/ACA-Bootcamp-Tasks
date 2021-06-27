22488243c03d5be767a92885041465e6ba38d79f
Arthur <gyulabyana@gmail.com>
2021-06-27 18:45:18 +0400

    test: testing api

--- 022-fetch-api/books/index.js
+++ 022-fetch-api/books/index.js
@@ -1,14 +1,34 @@
 const searchForm = document.querySelector('#search-form');
 const searchInput = document.querySelector('#query');
 const searchBtn = document.querySelector('#search-btn');
+const book = document.querySelector('.books-container');
 
+const renderDate = (data) => {
+    const title = document.createElement('h1');
+    title.textContent = data.title;
+    book.append(title);
+
+};
 
 const getData = (query) => {
     fetch(`http://openlibrary.org/search.json${query}`)
         .then(response => response.json())
-        .then(data => console.log(data));
+        .then(data => {
+            console.log(data.docs[2]);
+            renderDate(data.docs[2]);
+
+        });
 };
 
-getData(window.location.search);
+// getData(window.location.search);
+
+window.addEventListener('load', () => {
+    getData(window.location.search);
+});
+
+searchBtn.addEventListener('click', () => {
+    const query = window.location.search;
+    console.log(query);
+});
 
 
